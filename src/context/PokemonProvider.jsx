73b7b5be60da5fbb986d0ff5e9b886ebe956f664
Manuel <manuelcaicedo52@gmail.com>
2023-11-29 11:58:41 -0500

    continue provider

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -3,7 +3,13 @@ import React, { useState, useEffect } from 'react';
 
 const PokemonProvider = ({children}) => {
 
+  const [allPokemons, setAllPokemons] = useState([]);
+  onst [globalPokemons, setGlobalPokemons] = useState([]);
   const [offset, setOffset] = useState(0);
+
+  // Estados para la aplicación simples
+	const [loading, setLoading] = useState(true);
+	const [active, setActive] = useState(false);
 
   //call 50 pokemons for the API
   const getAllPokemons = async(limit = 50)=>{
@@ -13,12 +19,52 @@ const PokemonProvider = ({children}) => {
 
     const data = await res.json()
 
-    console.log(data)
-    //continuar video 29:21
+    const promises = data.results.map(async pokemon => {
+			const res = await fetch(pokemon.url);
+			const data = await res.json();
+			return data;
+		});
+    const results = await Promise.all(promises);
+    setAllPokemons([...allPokemons, ...results]);
+    setLoading(false)
   }
+
+  
+	//call all pokemons
+	const getGlobalPokemons = async () => {
+		const baseURL = 'https://pokeapi.co/api/v2/';
+
+		const res = await fetch(
+			`${baseURL}pokemon?limit=100000&offset=0`
+		);
+		const data = await res.json();
+
+		const promises = data.results.map(async pokemon => {
+			const res = await fetch(pokemon.url);
+			const data = await res.json();
+			return data;
+		});
+		const results = await Promise.all(promises);
+
+		setGlobalPokemons(results);
+		setLoading(false);
+	};
+
+  // call one pokemon for ID
+	const getPokemonByID = async id => {
+		const baseURL = 'https://pokeapi.co/api/v2/';
+
+		const res = await fetch(`${baseURL}pokemon/${id}`);
+		const data = await res.json();
+		return data;
+	};
 
   useEffect(() => {
 		getAllPokemons();
+	}, []);
+
+  useEffect(() => {
+		getGlobalPokemons();
 	}, []);
 
   return (
