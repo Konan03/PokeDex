0d96188b26124747c4c2ee0092f20ed2711b24ef
Manuel <manuelcaicedo52@gmail.com>
2023-11-29 22:55:05 -0500

    configure home and add all componets

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -1,5 +1,7 @@
 import { PokemonContext } from "./PokemonContext"
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
+import { useForm } from '../hook/useForm';
+
 
 const PokemonProvider = ({children}) => {
 
