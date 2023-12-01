58e36fcad9f3bc8ada3961de3f411d0fd23fcb26
Manuel <manuelcaicedo52@gmail.com>
2023-11-30 23:26:49 -0500

    add new icon home

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import {Link, Outlet, useNavigate } from 'react-router-dom'
 import { PokemonContext } from '../context/PokemonContext'
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 
 const Navigation = () => {
 
@@ -23,10 +24,7 @@ const Navigation = () => {
 		<h1>PokéDex Konan</h1>
         <header className='container'>
 				<Link to='/' className='logo'>
-					<img
-						src='https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png'
-						alt='Logo Pokedex'
-					/>
+					<HomeRoundedIcon className='iconStyle' style={{fontSize: '40px'}}/>
 				</Link>
 				<form onSubmit={onSearchSubmit}>
 					<div className='form-group'>
